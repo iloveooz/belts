e96bf413a98088252329fd0d5c6c078dc62da311
Sergey Piskunov <iloveooz@yandex.ru>
2022-02-09 18:09:05 +0700

    Update final_project.cpp

--- week5/final_project.cpp
+++ week5/final_project.cpp
@@ -5,14 +5,65 @@
 #include <vector>
 #include <string>
 
-class Date {
-public:
-	int GetYear() const;
-	int GetMonth() const;
-	int GetDay() const;
+enum class DaysInMonth {
+	January = 31,
+	February = 28,
+	March = 31,
+	April = 30,
+	May = 31,
+	June = 30,
+	July = 31,
+	August = 31,
+	September = 30,
+	October = 31,
+	November = 30,
+	December = 31
 };
 
-bool operator < (const Date& lhs, const Date& rhs);
+class Date {
+public:
+	Date() {
+		m_year = 0;
+		m_month = 0;
+		m_day = 0;
+	}
+
+	Date(int year, int month, int day) {
+		m_year = year;
+		m_month = month;
+		m_day = day;
+	}
+
+	int GetYear() const {
+		return m_year;
+	}
+
+	int GetMonth() const {
+		return m_month;
+	}
+
+	int GetDay() const {
+		return m_day;
+	}
+private:
+	int countDaysInDate() const {
+		
+	}
+
+	int m_year;
+	int m_month;
+	int m_day;
+};
+
+bool operator < (const Date& lhs, const Date& rhs) {
+	if (lhs.GetYear() < rhs.GetYear()) {
+		return true;
+	} else {
+		if (lhs.GetMonth() < rhs.GetMonth()) {
+
+		}
+	}
+}
 
 class Database {
 public:
