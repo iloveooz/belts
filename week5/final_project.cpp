cc8f0a28be1d94192d3b92f4ccc54487f2790505
Sergey Piskunov <iloveooz@yandex.ru>
2022-02-09 13:36:33 +0700

    Update final_project.cpp

--- week5/final_project.cpp
+++ week5/final_project.cpp
@@ -1,35 +1,41 @@
 // Реализуйте функции и методы классов и при необходимости добавьте свои
 
 #include <iostream>
+#include <map>
+#include <vector>
 #include <string>
 
 class Date {
 public:
-  int GetYear() const;
-  int GetMonth() const;
-  int GetDay() const;
+	int GetYear() const;
+	int GetMonth() const;
+	int GetDay() const;
 };
 
 bool operator<(const Date& lhs, const Date& rhs);
 
 class Database {
 public:
-  void AddEvent(const Date& date, const std::string& event);
-  bool DeleteEvent(const Date& date, const std::string& event);
-  int  DeleteDate(const Date& date);
+	void AddEvent(const Date& date, const std::string& event);
+	bool DeleteEvent(const Date& date, const std::string& event);
+	int  DeleteDate(const Date& date);
 
-  /* ??? */ Find(const Date& date) const;
-  
-  void Print() const;
+	void Find(const Date& date) const;
+
+	void Print() const;
+private:
+	std::map<Date, std::vector<std::string>> dbase;
 };
 
 int main() {
-  Database db;
+	Database db;
 
-  std::string command;
-  while (getline(std::cin, command)) {
-    // Считайте команды с потока ввода и обработайте каждую
-  }
+	std::string command;
+	while (getline(std::cin, command)) {
+		if (command == "exit" or command == "EXIT" or command == "quit" or command == "QUIT") {
+			break;
+		}
+	}
 
-  return 0;
+	return 0;
 }
