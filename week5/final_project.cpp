3608d0275746173f1e2b242adfc91dcf77dd87f3
Sergey Piskunov <iloveooz@yandex.ru>
2022-02-09 13:25:04 +0700

    Create final_project.cpp

---
+++ week5/final_project.cpp
@@ -0,0 +1,35 @@
+// Реализуйте функции и методы классов и при необходимости добавьте свои
+
+#include <iostream>
+#include <string>
+
+class Date {
+public:
+  int GetYear() const;
+  int GetMonth() const;
+  int GetDay() const;
+};
+
+bool operator<(const Date& lhs, const Date& rhs);
+
+class Database {
+public:
+  void AddEvent(const Date& date, const std::string& event);
+  bool DeleteEvent(const Date& date, const std::string& event);
+  int  DeleteDate(const Date& date);
+
+  /* ??? */ Find(const Date& date) const;
+  
+  void Print() const;
+};
+
+int main() {
+  Database db;
+
+  std::string command;
+  while (getline(std::cin, command)) {
+    // Считайте команды с потока ввода и обработайте каждую
+  }
+
+  return 0;
+}
