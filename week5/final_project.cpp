648a21809735b783e361a7814f59dce144fe342b
Sergey Piskunov <iloveooz@yandex.ru>
2022-02-11 00:01:00 +0700

    Update final_project.cpp

--- week5/final_project.cpp
+++ week5/final_project.cpp
@@ -2,147 +2,183 @@
 
 #include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <map>
 #include <vector>
 #include <string>
-
-enum Command {
-	req,
-	dat,
-	eve
-};
+#include <algorithm>
 
 class Date {
 public:
-	Date() {
-		m_year = 0;
-		m_month = 1;
-		m_day = 1;
-	}
+    Date() {
+        m_year = 0;
+        m_month = 1;
+        m_day = 1;
+    }
 
-	Date(int year, int month, int day) {
-		m_year = year;
-		m_month = month;
-		m_day = day;
-	}
+    Date(int year, int month, int day) {
+        m_year = year;
+        m_month = month;
+        m_day = day;
+    }
 
-	int GetYear() const {
-		return m_year;
-	}
+    int GetYear() const {
+        return m_year;
+    }
 
-	int GetMonth() const {
-		return m_month;
-	}
+    int GetMonth() const {
+        return m_month;
+    }
 
-	int GetDay() const {
-		return m_day;
-	}
+    int GetDay() const {
+        return m_day;
+    }
 
-	int countDaysInDate() const {
-		const int count = m_year * 365 + (m_month - 1) * 31 + m_day;
-		return count;
-	}
+    int countDaysInDate() const {
+        const int count = m_year * 365 + (m_month - 1) * 31 + m_day;
+        return count;
+    }
 private:
-	int m_year;
-	int m_month;
-	int m_day;
+    int m_year;
+    int m_month;
+    int m_day;
 };
 
 bool operator < (const Date& lhs, const Date& rhs) {
-	if (lhs.countDaysInDate() < rhs.countDaysInDate())
-		return true;
-	return false;
+    if (lhs.countDaysInDate() < rhs.countDaysInDate())
+        return true;
+    return false;
 }
 
 std::ostream& operator << (std::ostream& stream, const Date& rhs) {
-	stream <<
-		std::setw(4) << std::setfill('0') << rhs.GetYear() << '/' <<
-		std::setw(2) << std::setfill('0') << rhs.GetMonth() << '/' <<
-		std::setw(2) << std::setfill('0') << rhs.GetDay();
-	return stream;
+    stream <<
+           std::setw(4) << std::setfill('0') << rhs.GetYear() << '/' <<
+           std::setw(2) << std::setfill('0') << rhs.GetMonth() << '/' <<
+           std::setw(2) << std::setfill('0') << rhs.GetDay();
+    return stream;
 }
 
 class Database {
 public:
-	void AddEvent(const Date& date, const std::string& event);
-	bool DeleteEvent(const Date& date, const std::string& event);
-	int  DeleteDate(const Date& date);
+    void AddEvent(const Date& date, const std::string& event) {
+        dbase[date].push_back(event);
+        SortEvents(date);
+    }
 
-	void Find(const Date& date) const;
+    bool DeleteEvent(const Date& date, const std::string& event) {
+        if (std::count(dbase.at(date).begin(), dbase.at(date).end(), event) > 0) {
+            dbase.at(date).erase(
+                    std::remove_if(dbase.at(date).begin(), dbase.at(date).end(),
+                                   [&event] (const std::string& value) { return value == event; }), dbase.at(date).end());
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	void Print() const;
+    int  DeleteDate(const Date& date) {
+        size_t size = dbase.at(date).size();
+        if (dbase.count(date) > 0) {
+            dbase.erase(date);
+        }
+        return size;
+    }
+
+    void Find(const Date& date) const {
+        
+    }
+
+    void Print() const {
+        for (const auto& [key, value] : dbase) {
+            std::cout << key << ' ';
+            for (const auto& item : value) {
+                std::cout << item << ' ';
+            }
+            std::cout << std::endl;
+        }
+    }
+
 private:
-	std::map<Date, std::vector<std::string>> dbase;
+    void SortEvents(const Date& date) {
+        std::sort(dbase.at(date).begin(), dbase.at(date).end());
+    }
+    std::map<Date, std::vector<std::string>> dbase;
 };
 
-std::string ParseCommand(const std::string& str, Command com) {
-	std::string result;
-	size_t pos1 = str.find(' ');
-	size_t pos2 = str.find(' ');
+std::vector<std::string> ParseCommand(const std::string& str) {
+    std::vector<std::string> result;
+    char delimiter = ' ';
+    std::string word;
 
-	switch (com) {
-		case req:			
-			if (pos1 != std::string::npos)
-				result = str.substr(0, pos1);
-			break;
-		case dat:
-			if (pos1 != std::string::npos && pos2 != std::string::npos)
-				result = str.substr(pos1 + 1, pos2);
-			break;
-		case eve:
-			if (pos2 != std::string::npos)
-				result = str.substr(pos2 + 1);
-			break;
-		default: 
-			break;
-	}
+    std::stringstream sstream(str);
 
-	return result;
+    while(std::getline(sstream, word, delimiter)) {
+        result.push_back(word);
+    }
+
+    return result;
+}
+
+Date ParseDate(const std::string& sDate) {
+    std::vector<std::string> date_parts;
+    char delimiter = '-';
+    std::string word;
+
+    std::stringstream sstream(sDate);
+
+    while(std::getline(sstream, word, delimiter)) {
+        date_parts.push_back(word);
+    }
+
+    Date date(std::stoi(date_parts[0]), std::stoi(date_parts[1]), std::stoi(date_parts[2]));
+
+    return date;
 }
 
 int main() {
-	Database db;
+    Database db;
 
-	std::string command; // request date event
-	std::string request; // add del find print
-	std::string date;    // year-month-day
-	std::string event;   // event
+    std::string command; // request date event
+    std::vector<std::string> request; // add del find print
+    Date date;
 
 
-	while (getline(std::cin, command)) {
-		request = ParseCommand(command, req);
-		date = ParseCommand(command, dat);
-		event = ParseCommand(command, eve);
+    while (getline(std::cin, command)) {
+        if (!command.empty()) {
+            request = ParseCommand(command);
 
-		if (request == "Add") {
-			std::cout << request << std::endl;
-			std::cout << date << std::endl;
-			std::cout << event << std::endl;
-		}
+            if (request[0] == "Add") {
+                date = ParseDate(request[1]);
+                db.AddEvent(date, request[2]);
+            }
+            else if (request[0] == "Del") {
+                date = ParseDate(request[1]);
+                if (request.size() == 3) {
+                    if (db.DeleteEvent(date, request[2])) {
+                        std::cout << "Deleted successfully" << std::endl;
+                    } else {
+                        std::cout << "Event not found" << std::endl;
+                    }
+                } else {
+                    std:: cout << "Deleted " << db.DeleteDate(date) << " events";
+                }
+            }
+            else if (request[0] == "Find") {
+                date = ParseDate(request[1]);
+                db.Find(date);
+            }
+            else if (request[0] == "Print") {
+                db.Print();
+            }
+            else {
+                std::cout << "Unknown command: " << command << std::endl;
+            }
 
-		if (request == "Del") {
-			std::cout << request << std::endl;
-			std::cout << date << std::endl;
-			std::cout << event << std::endl;
-		}
+            if (command == "q" or command == "Q") {
+                break;
+            }
+        }
+    }
 
-		if (request == "Find") {
-			std::cout << request << std::endl;
-			std::cout << date << std::endl;
-			std::cout << event << std::endl;
-		}
-
-		if (request == "Print") {
-			std::cout << request << std::endl;
-			std::cout << date << std::endl;
-			std::cout << event << std::endl;
-		}
-
-		if (command == "q" or command == "Q") {
-			break;
-		}
-	}
-
-	system("pause");
-	return 0;
+    return 0;
 }
